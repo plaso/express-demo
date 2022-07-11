ed95a7d52cf67c2432b6dbc5096277ae9483bc86
Cdelatorre <carlosdelatorrediaz@gmail.com>
2022-07-11 21:33:30 +0200

    add fetch and promise behavioir with hbs and controllers

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const express = require('express')
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
 
-app.use(express.static('public'))
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
-app.get('/', (req, res, next) => {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/views/index.html')
-})
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.get('/marco', (req, res, next) => {
-  console.log(req)
-  res.send('Esto ta chupao')
-})
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname + '/views/notFound.html')
-})
+app.get("/users", (req, res, next) => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      res.render("users", { users });
+    });
+});
+
+app.get("/users/:id", (req, res, next) => {
+  const idParam = req.params.id;
+
+  fetch(`https://jsonplaceholder.typicode.com/users/${idParam}`)
+    .then((response) => response.json())
+    .then((user) => {
+      res.render("detail", { user });
+    });
+});
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+  console.log("Listening on port 3000");
+});
